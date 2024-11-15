7a9c535309fa3aa18333e568255c5bc9ddfb1ed4
Jamie Lee <jamielee@Jamies-MacBook-Pro.local>
2024-11-14 19:50:17 -0500

    committing changes to 03-clean_data.R script

--- scripts/03-clean_data.R
+++ scripts/03-clean_data.R
@@ -7,20 +7,22 @@
 # Pre-requisites: Raw data obtained from Project Hammer in SQLite file and cleaned with SQL in DB Browser
 # Any other information needed? N/A
 
-SELECT 
-  STRFTIME('%Y-%m-%d %H:%M:%S', nowtime) AS nowtime, 
-  product_id,
-  current_price,
-  old_price,
-  other,
-  product_name,
-  vendor
-FROM 
-  raw
-JOIN 
-  product ON product_id = id
-WHERE vendor = 'Loblaws'
-  AND product_id IS NOT NULL
-  AND current_price IS NOT NULL
-  AND old_price IS NOT NULL
-  AND product_name IS NOT NULL;
+# Below is the SQL script I used on DB Browser to clean and manipulate the raw data obtained from Project Hammer.
+
+#SELECT 
+  #STRFTIME('%Y-%m-%d %H:%M:%S', nowtime) AS nowtime, 
+  #product_id,
+  #current_price,
+  #old_price,
+  #other,
+  #product_name,
+  #vendor
+#FROM 
+  #raw
+#JOIN 
+  #product ON product_id = id
+#WHERE vendor = 'Loblaws'
+  #AND product_id IS NOT NULL
+  #AND current_price IS NOT NULL
+  #AND old_price IS NOT NULL
+  #AND product_name IS NOT NULL;
